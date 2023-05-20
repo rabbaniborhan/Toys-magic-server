699efd8441fdd0b41495a33b6513b588869ec4a5
Borhan <brborhan70@gmail.com>
2023-05-20 17:44:58 +0600

    toys data post function complate

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-
 app.use(cors());
 app.use(express.json());
 
@@ -13,7 +12,8 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.msnuvxp.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,12 +26,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const db =client.db("toysMaker");
+    const toysCollection = db.collection("toys");
+
+
+
+    app.post("/add-toys",async(req,res)=>{
+      const body = req.body;
+      const result =await toysCollection.insertOne(body);
+
+     res.send(result)
+
+      console.log(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
